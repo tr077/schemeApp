10e0ef9a350cb7ed8d04176c0b8b9ddd691c4790
Joanna Hemmingsson <joanna.hemmingsson@gmail.com>
2013-04-25 13:36:16 +0200

    Initial Commit

---
+++ connection/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  connection
+//
+//  Created by Joanna Hemmingsson on 2013-04-25.
+//  Copyright (c) 2013 Joanna Hemmingsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
