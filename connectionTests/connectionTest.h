d3c2caffbf7dca79fdb00f7227c845442e9a83ee
Joanna Hemmingsson <joanna.hemmingsson@gmail.com>
2013-05-02 16:31:20 +0200

    added some functionallity

---
+++ connectionTests/connectionTest.h
@@ -0,0 +1,13 @@
+//
+//  connectionTest.h
+//  connection
+//
+//  Created by Joanna Hemmingsson on 2013-04-30.
+//  Copyright (c) 2013 Joanna Hemmingsson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface connectionTest : SenTestCase
+
+@end
---
+++ connectionTests/connectionTest.m
@@ -0,0 +1,13 @@
+//
+//  connectionTest.m
+//  connection
+//
+//  Created by Joanna Hemmingsson on 2013-04-30.
+//  Copyright (c) 2013 Joanna Hemmingsson. All rights reserved.
+//
+
+#import "connectionTest.h"
+
+@implementation connectionTest
+
+@end
